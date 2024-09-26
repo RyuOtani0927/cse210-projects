e859ea1440122ccf515564d1bde3d4df9cdcfac6
Ryu Otani <ryuotani@OTA-Book.local>
2024-09-26 17:10:07 -0600

    Assignment Finished.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine();
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int num = PromptUserNumber();
+
+        int square = SquareNumber(num);
+
+        DisplayResult(name,square);
+
+        Console.WriteLine();
     }
 }
