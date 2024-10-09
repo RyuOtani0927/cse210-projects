256394d992874afd61f54ea41ce4824214f7202d
Ryu Otani <ryuotani@OTA-Book.local>
2024-10-09 14:37:46 -0600

    Little Update.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,7 +3,7 @@
 // Ryu Otani
 
 // Exceeding Requirements
-// 1. It keeps asking, if the first response is not between 1-5.
+// 1. It keeps asking what the user would like to do, if the response is not between 1-5.
 // 2. Added one more information "score" in addition to date, prompt, and response.
 
 
