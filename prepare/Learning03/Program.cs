6bf6a9a2513028de4365abaebdfe64b0d4704d27
Ryu Otani <ryuotani@OTA-Book.local>
2024-10-18 15:13:55 -0600

    Assignment Finished.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction num1 = new Fraction();
+        Fraction num2 = new Fraction(5);
+        Fraction num3 = new Fraction(3,4);
+        Fraction num4 = new Fraction(1,3);
+
+        Console.WriteLine(num1.GetFractionString());
+        Console.WriteLine(num1.GetDecimalValue());
+        Console.WriteLine(num2.GetFractionString());
+        Console.WriteLine(num2.GetDecimalValue());
+        Console.WriteLine(num3.GetFractionString());
+        Console.WriteLine(num3.GetDecimalValue());
+        Console.WriteLine(num4.GetFractionString());
+        Console.WriteLine(num4.GetDecimalValue());
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,44 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Costume nurse = new Costume();
-        nurse.headWear = "face mask";
-        nurse.gloves = "nitrile gloves";
-        nurse.shoes = "orthopedic sneakers";
-        nurse.upperGarmet = "scrubs";
-        nurse.lowerGarment = "scrubs";
-        nurse.accessory = "stethoscope";
+        
+        Bin myBin = new("quarters",40,(float)0.25);
 
-        Costume detective = new Costume();
-        detective.headWear = "ffedora";
-        detective.gloves = "leather";
-        detective.shoes = "loafers";
-        detective.upperGarmet = "trenchcoat";
-        detective.lowerGarment = "slacks";
-        detective.accessory = "magnifying glass";
-
-        Costume rancher = new Costume();
-        rancher.headWear = "cowboy hat";
-        rancher.gloves = "work";
-        rancher.shoes = "boots";
-        rancher.upperGarmet = "fancy vest";
-        rancher.lowerGarment = "jeans";
-        rancher.accessory = "lasso";
-
-        Console.WriteLine("Nurse Costume: ");
-        nurse.ShowWardrobe();
-
-        Console.WriteLine();
-
-        Console.WriteLine("Detective Costume: ");
-        detective.ShowWardrobe();
-
-        Console.WriteLine();
-
-        Console.WriteLine("Rancher Costume: ");
-        rancher.ShowWardrobe();
-
-        Console.WriteLine();
+        myBin.ModifyAmount(6);
+        Console.WriteLine(myBin.TotalValue());
     }
 
 }
