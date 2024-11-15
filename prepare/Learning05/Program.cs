e9c436703d863b18e3b6ef099fd67bdf07093c86
Ryu Otani <ryuotani@OTA-Book.local>
2024-11-15 15:54:13 -0700

    Assignment Finished.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new();
+
+        shapes.Add(new Square("red",5));
+        shapes.Add(new Rectangle("yellow",2,10));
+        shapes.Add(new Circle("blue",3));
+
+        Console.WriteLine();
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
